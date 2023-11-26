63bc34b2be4cc7eb61c967796e9d06de6828d604
Ameeth Manoj Bhuvanapalli <manojba04@gmail.com>
2023-11-26 15:51:27 +0530

    added validations

--- static/functions.js
+++ static/functions.js
@@ -29,3 +29,21 @@ function openDialog(auctionId,auctionText) {
 function closeDialog() {
     document.getElementById('bidsDialog').close();
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get today's date in the format expected by datetime-local input
+    var today = new Date().toISOString().slice(0, 16);
+    // Set the minimum value for the Start Date input
+    var startDateInput = document.getElementsByName("startDate")[0];
+    startDateInput.min = today;
+
+    // Set up event listener for Start Date input
+    startDateInput.addEventListener("input", function () {
+        var startDate = new Date(startDateInput.value);
+        startDate.setMinutes(startDate.getMinutes() + 24 * 60 + 5 * 60 + 30);
+        var endDate = startDate.toISOString().slice(0, 16);
+        var endDateInput = document.getElementsByName("endDate")[0];
+        endDateInput.value = endDate;
+        endDateInput.min = endDate;
+    });
+});
